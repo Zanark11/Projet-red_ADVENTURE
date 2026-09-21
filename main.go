3d1a2679e3a6850a11532a1e919fb829a29c3356
TOURE Rayane <rayane.toure@ynov.com>
2026-09-21 10:58:01 +0200

    print

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 
 	maxSlots := 5
 
+	// ============================================================
+	// MARCHAND
+	// ============================================================
+
+	println("=== MARCHAND ===")
 	println("Pièces au départ :", player.Gold)
 
 	if Buy(&player, &inventory, item, maxSlots) {
@@ -22,4 +27,42 @@ func main() {
 
 	println("Pièces restantes :", player.Gold)
 	println("Objets dans l'inventaire :", len(inventory))
+
+	// ============================================================
+	// ÉQUIPEMENTS
+	// ============================================================
+
+	slots := EquipmentSlots{}
+
+	head := Equipment{
+		Name:    "Chapeau de l'aventurier",
+		Slot:    "Tête",
+		BonusHP: 10,
+	}
+
+	chest := Equipment{
+		Name:    "Tunique de l'aventurier",
+		Slot:    "Torse",
+		BonusHP: 20,
+	}
+
+	feet := Equipment{
+		Name:    "Bottes de l'aventurier",
+		Slot:    "Pieds",
+		BonusHP: 10,
+	}
+
+	// Équiper les objets
+	Equip(&slots, head)
+	Equip(&slots, chest)
+	Equip(&slots, feet)
+
+	println()
+	println("=== ÉQUIPEMENTS ===")
+
+	println("Tête :", slots.Head.Name)
+	println("Torse :", slots.Chest.Name)
+	println("Pieds :", slots.Feet.Name)
+
+	println("Bonus total de PV :", TotalBonusHP(&slots))
 }
