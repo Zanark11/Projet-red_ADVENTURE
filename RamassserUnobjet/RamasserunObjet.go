58e9dee13c35c58a0ad9a144850294f9fa8f5774
kthieryh <thieryhenri.kouame@ynov.com>
2026-09-22 13:55:25 +0200

    print

--- RamassserUnobjet/RamasserunObjet.go
+++ RamassserUnobjet/RamasserunObjet.go
@@ -1,6 +1,14 @@
 package ramassserunobjet
 
+import (
+	"aventure/personnage"
+	"fmt"
+)
 
-func RamasserUnobjet(){
-	
+func RamasserUnobjet(player *personnage.Character, objet string ) {
+	if HasInventorySpace(player){
+		AddItem(player, Item{nom:objet})
+	}else {
+		fmt.Println("votre invenraire est plein")
+	}
 }
