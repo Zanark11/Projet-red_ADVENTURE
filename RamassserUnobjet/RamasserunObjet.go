22e490b41451853a8afecebd9cd91771e6c817bd
kthieryh <thieryhenri.kouame@ynov.com>
2026-09-22 14:38:09 +0200

    print

--- RamassserUnobjet/RamasserunObjet.go
+++ RamassserUnobjet/RamasserunObjet.go
@@ -2,13 +2,43 @@ package ramassserunobjet
 
 import (
 	"aventure/personnage"
+	"aventure/marchand"
 	"fmt"
 )
-
-func RamasserUnobjet(player *personnage.Character, objet string ) {
-	if HasInventorySpace(player){
-		AddItem(player, Item{nom:objet})
-	}else {
-		fmt.Println("votre invenraire est plein")
+/*code pour l'appeler dans explorer pour que le joueur ait la possibilitée de ramaser comme de réfuser*/
+func RamasserUnobjet(player *personnage.Character, objet string) {
+	var choix int 
+	for i, objetInventaire := range player.Inventaire {
+		if objetInventaire == "" {
+			player.Inventaire[i] = objet
+			return
+		}
 	}
+	fmt.Println("votre invenraire est plein")
+	fmt.Println("voulez vous augmenter votre inventaire ?")
+	fmt.Println()
+	fmt.Println("1.Oui")
+	fmt.Println("2.Non")
+
+	fmt.Scanln(&choix)
+	switch choix {
+	case 1 :
+		if marchand.AugmenterInventaire(player){
+            fmt.Println("votre inventaire a augmenté !")
+			for i, objetInventaire := range player.Inventaire {
+		            if objetInventaire == "" {
+			            player.Inventaire[i] = objet
+						break
+					}
+				}
+
+		    }else {
+                fmt.Println("vous n'avez pas assez d'argent")
+		    }
+	case 2 :
+		fmt.Println("vous laissez l'objet !")
+		return
+	default:
+		fmt.Println("choix invalide")
+	}    
 }
